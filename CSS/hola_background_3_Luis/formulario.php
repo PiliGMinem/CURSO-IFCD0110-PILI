54b81775764282bf4731d93db04fd97bd35f09e3
PiliGMinem <mariadelpilargarciamirasinem@gmail.com>
2022-07-08 10:26:13 +0200

    commit moví proyecto Luis y está el formulario.php bien

--- CSS/hola_background_3_Luis/formulario.php
+++ CSS/hola_background_3_Luis/formulario.php
@@ -5,19 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--FavIcon el que se ve en la pestaña del navegador -->
-    <link rel="icon" href="img/promotv144.png">
     <title>Hola Background</title>
-    <!--Agregamos nuestro CSS-->
     <link href="css/index.css" rel="stylesheet" />
-    <!--Agregamos Font Awesome desde un CDN. Tamb ién se puede hacer por @import en CSS-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" />
+
 </head>
 
 <body>
-    <!-- Header. esto se insertará dinámicamente con Javascript -->
     <header>
-        <!--Etiqueta semántica para añadir un menú de navegación de la página -->
+
         <nav>
             <a href="index.html">
                 <i class="fa-solid fa-house"> </i> Inicio
@@ -31,14 +27,17 @@
         </nav>
     </header>
     <div class="background">
-  
+
         <!--
         Background igual que el de index
     -->
     </div>
-     
+
     <?php
-    echo "HOLA MUNDO";
+        echo "<br><br><br><hr><h1>Hola Mundo</h1>";
+        // echo json_encode($_SERVER);
+        echo "<h2>Me has llamado por el método ".$_SERVER['REQUEST_METHOD']."</h2>";
     ?>
 </body>
+
 </html>
