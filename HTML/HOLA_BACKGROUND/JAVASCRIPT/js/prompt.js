a9f4498397e8a8066727b37c39f156ed27bc04ee
PiliGMinem <mariadelpilargarciamirasinem@gmail.com>
2022-07-21 13:38:19 +0200

    con javascript

---
+++ HTML/HOLA_BACKGROUND/JAVASCRIPT/js/prompt.js
@@ -0,0 +1,26 @@
+// Vamos a realizar una entrada de datos
+// y darle salida por la pantalla con un alert();
+
+
+// declaramos variable
+let edad;
+
+// Le damos un valor a variable.
+// En este caso le damos un valor que viene de un return
+edad = prompt('Introduce tu edad');
+// Creamos un condicional para saber si nos han escrito un número
+
+if(Number(edad) == edad){
+    // Escribimos el resultado
+    document.getElementById('main').innerHTML= `
+    <h2>Enhorabuena!! Tu edad es </h2>
+    <h1>${edad}</h1>
+`;
+
+}else{
+    // o es un número, avisamos,
+    alert('Tiene que ser un número');
+    // Recargamos para que vuelva a salir el prompt
+    window.location.reload();
+}
+
