0c1304425953c56a26f0c85b4e2ee9013c6791b9
PiliGMinem <mariadelpilargarciamirasinem@gmail.com>
2022-08-29 12:02:41 +0200

    commit con login.php y bd de mysql

---
+++ LOGINPHP/form.php
@@ -0,0 +1,27 @@
+<?php
+     ini_set('display_errors', '1');
+     ini_set('display_startup_errors', '1');
+     error_reporting(E_ALL);
+     header("Access-Control-Allow-Origin: *");
+     header("Content-Type: application/json; charset=UTF-8");
+     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FETCH, OPTIONS");
+     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    //Recogemos todos los datos enviado por el usuario ...
+     $data = json_decode(file_get_contents('php://input'), true);
+     // El nombre de usuario que debe ser 
+    $userguardado="ablaracurcix";
+    //El password que debe ser
+    $passwordguardado="pinpanpum";
+    //Extraemos de los datos el dato de nombre de usuario
+    $user = $data["user"];
+    //Extraemos el password enviado por el cliente
+    $password = $data["password"];
+    //Comprobamos que coinciden
+    //Si no lo hacen
+    if ($user == $userguardado && $password==$passwordguardado){
+        echo "enhorabuena";
+        exit;
+    }else{
+        http_response_code(401);
+    }
+    exit;
--- LOGINPHP/login.php
+++ LOGINPHP/login.php
@@ -1,20 +1,23 @@
 <?php
-     ini_set('display_errors', '1');
+     /*ini_set('display_errors', '1');
      ini_set('display_startup_errors', '1');
-     error_reporting(E_ALL);
+     error_reporting(E_ALL);*/
      header("Access-Control-Allow-Origin: *");
      header("Content-Type: application/json; charset=UTF-8");
      header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FETCH, OPTIONS");
      header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    //Recogemos todos los datos enviado por el usuario ...
      $data = json_decode(file_get_contents('php://input'), true);
-    $userguardado="ablaracurcix";
-    $passwordguardado="pinpanpum";
-    $user = $data["user"];
-    $password = $data["password"];
-    if ($user == $userguardado && $password==$passwordguardado){
-        echo "enhorabuena";
-        exit;
-    }else{
+     
+     //VAlores de conexión a la base de datos
+     $server = 'localhost';
+     $user = 'ifcd0110';
+     $pass='pili2022@';
+     $db = 'clase';
+     if(!mysqli_connect($server, $user, $pass, $db)){
+        echo '<h1>ERROR, no se ha podido conectar a la base de datos</h1>';
         http_response_code(401);
-    }
-    exit;
+        exit;
+     }
+     echo "<h1>ENHORABUENA, te has conectado a la base de datos</h1>";
+     exit;
